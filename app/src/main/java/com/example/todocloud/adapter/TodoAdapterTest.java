b30de586dfdf38f6e94949135cb841ba0b4b7b5e
drpcdoki <vitezroland91@gmail.com>
2017-06-26 15:44:50 +0200

    Add TodoAdapterTest class
    It's purpose is: display an item of TodoListFragmentTest. It's currently
    under implementation and testing.

--- app/src/main/java/com/example/todocloud/adapter/TodoAdapterTest.java
+++ app/src/main/java/com/example/todocloud/adapter/TodoAdapterTest.java
@@ -1,4 +1,48 @@
 package com.example.todocloud.adapter;
 
-public class TodoAdapterTest {
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.todocloud.R;
+import com.example.todocloud.data.Todo;
+
+import java.util.List;
+
+public class TodoAdapterTest extends RecyclerView.Adapter<TodoAdapterTest.MyViewHolder> {
+
+  private List<Todo> todosList;
+
+  @Override
+  public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    View itemView = LayoutInflater.from(parent.getContext())
+        .inflate(R.layout.todo_item, parent, false);
+
+    return new MyViewHolder(itemView);
+  }
+
+  @Override
+  public void onBindViewHolder(MyViewHolder holder, int position) {
+    Todo todo = todosList.get(position);
+    holder.title.setText(todo.getTitle());
+  }
+
+  @Override
+  public int getItemCount() {
+    return todosList.size();
+  }
+
+  public class MyViewHolder extends RecyclerView.ViewHolder {
+
+    public TextView title;
+
+    public MyViewHolder(View itemView) {
+      super(itemView);
+      title = (TextView) itemView.findViewById(R.id.tvTitle);
+    }
+
+  }
+
 }
