58ef6a17fb070820974f22efc4b82caa88309c4e
drpcdoki <vitezroland91@gmail.com>
2017-06-26 12:00:29 +0200

    Add TodoListFragmentTest class
    It's purpose is: implement the current TodoListFragment class based on
    RecyclerView, instead of ListView. It's currently under implementation
    and testing.

---
+++ app/src/main/java/com/example/todocloud/fragment/TodoListFragmentTest.java
@@ -0,0 +1,12 @@
+package com.example.todocloud.fragment;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+
+public class TodoListFragmentTest extends RecyclerView {
+
+  public TodoListFragmentTest(Context context) {
+    super(context);
+  }
+
+}
