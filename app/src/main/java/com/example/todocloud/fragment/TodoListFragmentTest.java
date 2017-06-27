e5a47ea7d99de3eaa809dbe82839af3bdd5ca079
drpcdoki <vitezroland91@gmail.com>
2017-06-27 08:40:58 +0200

    Modify inheritance of TodoListFragmentTest class
    It was mistakenly inherited from RecyclerView class, instead of Fragment.

--- app/src/main/java/com/example/todocloud/fragment/TodoListFragmentTest.java
+++ app/src/main/java/com/example/todocloud/fragment/TodoListFragmentTest.java
@@ -1,12 +1,6 @@
 package com.example.todocloud.fragment;
 
-import android.content.Context;
-import android.support.v7.widget.RecyclerView;
+import android.support.v4.app.Fragment;
 
-public class TodoListFragmentTest extends RecyclerView {
-
-  public TodoListFragmentTest(Context context) {
-    super(context);
-  }
-
+public class TodoListFragmentTest extends Fragment {
 }
