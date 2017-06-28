811c77ed0312c97303f27f3b48ca84e69116f043
pcdoki <vitezroland91@gmail.com>
2017-06-28 10:22:48 +0200

    Implement handling all of the components

--- app/src/main/java/com/example/todocloud/adapter/TodoAdapterTest.java
+++ app/src/main/java/com/example/todocloud/adapter/TodoAdapterTest.java
@@ -1,9 +1,11 @@
 package com.example.todocloud.adapter;
 
+import android.support.v7.widget.AppCompatCheckBox;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.todocloud.R;
@@ -28,7 +30,7 @@ public class TodoAdapterTest extends RecyclerView.Adapter<TodoAdapterTest.MyView
   @Override
   public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
     View itemView = LayoutInflater.from(parent.getContext())
-        .inflate(R.layout.todo_item, parent, false);
+        .inflate(R.layout.todo_item_test, parent, false);
 
     return new MyViewHolder(itemView);
   }
@@ -36,7 +38,10 @@ public class TodoAdapterTest extends RecyclerView.Adapter<TodoAdapterTest.MyView
   @Override
   public void onBindViewHolder(MyViewHolder holder, int position) {
     Todo todo = todosList.get(position);
+    holder.completed.setChecked(todo.getCompleted());
     holder.title.setText(todo.getTitle());
+    holder.dueDate.setText(todo.getDueDate());
+    holder.priority.setVisibility(todo.isPriority() ? View.VISIBLE : View.INVISIBLE);
   }
 
   @Override
@@ -46,11 +51,17 @@ public class TodoAdapterTest extends RecyclerView.Adapter<TodoAdapterTest.MyView
 
   public class MyViewHolder extends RecyclerView.ViewHolder {
 
+    public AppCompatCheckBox completed;
     public TextView title;
+    public TextView dueDate;
+    public ImageView priority;
 
     public MyViewHolder(View itemView) {
       super(itemView);
-      title = (TextView) itemView.findViewById(R.id.tvTitle);
+      completed = (AppCompatCheckBox) itemView.findViewById(R.id.completed);
+      title = (TextView) itemView.findViewById(R.id.title);
+      dueDate = (TextView) itemView.findViewById(R.id.dueDate);
+      priority = (ImageView) itemView.findViewById(R.id.priority);
     }
 
   }
