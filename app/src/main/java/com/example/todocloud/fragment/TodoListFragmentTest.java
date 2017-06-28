f105ccf7b9bea7ceef2dc5aca29e9a6ff308f932
pcdoki <vitezroland91@gmail.com>
2017-06-28 14:46:44 +0200

    Implement CreateTodo function for AppBar

--- app/src/main/java/com/example/todocloud/fragment/TodoListFragmentTest.java
+++ app/src/main/java/com/example/todocloud/fragment/TodoListFragmentTest.java
@@ -1,31 +1,46 @@
 package com.example.todocloud.fragment;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.view.ActionMode;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.todocloud.R;
 import com.example.todocloud.adapter.TodoAdapterTest;
 import com.example.todocloud.data.Todo;
+import com.example.todocloud.datastorage.DbConstants;
 import com.example.todocloud.datastorage.DbLoader;
 import com.example.todocloud.datastorage.asynctask.UpdateAdapterTask;
+import com.example.todocloud.helper.OnlineIdGenerator;
+import com.example.todocloud.service.AlarmService;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class TodoListFragmentTest extends Fragment {
+public class TodoListFragmentTest extends Fragment implements
+    TodoCreateFragment.ITodoCreateFragment {
 
   private DbLoader dbLoader;
   private List<Todo> todosList = new ArrayList<>();
   private TodoAdapterTest todoAdapterTest;
   private RecyclerView recyclerView;
+  private ITodoListFragmentTest listener;
+
+  @Override
+  public void onAttach(Context context) {
+    super.onAttach(context);
+    listener = (ITodoListFragmentTest) context;
+  }
 
   @Override
   public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -58,6 +73,98 @@ public class TodoListFragmentTest extends Fragment {
   @Override
   public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
     inflater.inflate(R.menu.todo_test_options_menu, menu);
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    int optionsItemId = item.getItemId();
+
+    switch (optionsItemId) {
+      case R.id.createTodo:
+        listener.openTodoCreateFragment(this);
+        break;
+    }
+
+    return super.onOptionsItemSelected(item);
+  }
+
+  @Override
+  public void onCreateTodo(Todo todoToCreate) {
+    createTodoInLocalDatabase(todoToCreate);
+    updateTodoAdapterTest();
+
+    if (isSetReminder(todoToCreate) && isNotCompleted(todoToCreate)) {
+      createReminderService(todoToCreate);
+    }
+  }
+
+  private void createTodoInLocalDatabase(Todo todoToCreate) {
+    Bundle arguments = getArguments();
+
+    if (isPredefinedListCompleted(arguments)) {
+      todoToCreate.setCompleted(true);
+    }
+
+    String listOnlineId = arguments.getString("listOnlineId");
+    if (!isPredefinedList(listOnlineId)) {
+      todoToCreate.setListOnlineId(listOnlineId);
+    }
+
+    todoToCreate.setUserOnlineId(dbLoader.getUserOnlineId());
+    todoToCreate.set_id(dbLoader.createTodo(todoToCreate));
+    String todoOnlineId = OnlineIdGenerator.generateOnlineId(
+        getActivity(),
+        DbConstants.Todo.DATABASE_TABLE,
+        todoToCreate.get_id(),
+        dbLoader.getApiKey()
+    );
+    todoToCreate.setTodoOnlineId(todoOnlineId);
+    dbLoader.updateTodo(todoToCreate);
+  }
+
+  private boolean isPredefinedListCompleted(Bundle arguments) {
+    String selectFromArguments = arguments.getString("selectFromDB");
+    String selectPredefinedListCompleted =
+        DbConstants.Todo.KEY_COMPLETED +
+            "=" +
+            1 +
+            " AND " +
+            DbConstants.Todo.KEY_USER_ONLINE_ID +
+            "='" +
+            dbLoader.getUserOnlineId() +
+            "'" +
+            " AND " +
+            DbConstants.Todo.KEY_DELETED +
+            "=" +
+            0;
+
+    return selectFromArguments != null && selectFromArguments.equals(selectPredefinedListCompleted);
+  }
+
+  private boolean isPredefinedList(String listOnlineId) {
+    return listOnlineId == null;
+  }
+
+  private boolean isSetReminder(Todo todo) {
+    return !todo.getReminderDateTime().equals("-1");
+  }
+
+  private boolean isNotCompleted(Todo todo) {
+    return !todo.getCompleted();
+  }
+
+  private void createReminderService(Todo todo) {
+    Intent reminderService = new Intent(getActivity(), AlarmService.class);
+    reminderService.putExtra("todo", todo);
+    reminderService.setAction(AlarmService.CREATE);
+    getActivity().startService(reminderService);
+  }
+
+  public interface ITodoListFragmentTest {
+    void setActionBarTitle(String actionBarTitle);
+    void startActionMode(ActionMode.Callback callback);
+    void onTodoClicked(Todo clickedTodo, TodoListFragmentTest targetFragment);
+    void openTodoCreateFragment(TodoListFragmentTest targetFragment);
   }
 
 }
