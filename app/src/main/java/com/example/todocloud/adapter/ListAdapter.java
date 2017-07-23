aed63ec2344f8b9fdca8e3e499591caa400c0153
rolandvitezhu <vitezroland91@gmail.com>
2017-07-23 21:07:10 +0200

    Simplify ListAdapter class

--- app/src/main/java/com/example/todocloud/adapter/ListAdapter.java
+++ app/src/main/java/com/example/todocloud/adapter/ListAdapter.java
@@ -13,59 +13,44 @@ import java.util.List;
 
 public class ListAdapter extends BaseAdapter {
 
-  private List<com.example.todocloud.data.List> listItems;
+  private List<com.example.todocloud.data.List> lists;
 
-  public ListAdapter(List<com.example.todocloud.data.List> listItems) {
-    this.listItems = listItems;
+  public ListAdapter(List<com.example.todocloud.data.List> lists) {
+    this.lists = lists;
   }
 
   @Override
   public int getCount() {
-    return listItems.size();
+    return lists.size();
   }
 
   @Override
   public Object getItem(int position) {
-    return listItems.get(position);
+    return lists.get(position);
   }
 
   @Override
   public long getItemId(int position) {
-    return listItems.get(position).get_id();
-  }
-
-  public String getItemListOnlineId(int position) {
-    return listItems.get(position).getListOnlineId();
+    com.example.todocloud.data.List list = lists.get(position);
+    return list.get_id();
   }
 
   @Override
   public View getView(int position, View convertView, ViewGroup parent) {
-    final com.example.todocloud.data.List listItem = listItems.get(position);
-    LayoutInflater inflater = (LayoutInflater) parent.getContext().
-        getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-    convertView = inflater.inflate(R.layout.list_item, null);
+    com.example.todocloud.data.List list = lists.get(position);
+    LayoutInflater layoutInflater = (LayoutInflater) parent.getContext().getSystemService(
+        Context.LAYOUT_INFLATER_SERVICE
+    );
+    convertView = layoutInflater.inflate(R.layout.list_item, null);
     TextView tvTitle = (TextView) convertView.findViewById(R.id.tvTitle);
-    tvTitle.setText(listItem.getTitle());
+    tvTitle.setText(list.getTitle());
+
     return convertView;
   }
 
-  /**
-   * Törli az adapterből a megadott List-et .
-   * @param list A törlendő List.
-   */
-  public void deleteList(com.example.todocloud.data.List list) {
-    if (listItems.contains(list)) {
-      listItems.remove(list);
-    }
-  }
-
-  /**
-   * Frissíti az adapter tartalmát a megadott adatokkal.
-   * @param listItems A megadott List-ek.
-   */
-  public void update(List<com.example.todocloud.data.List> listItems) {
-    this.listItems.clear();
-    this.listItems.addAll(listItems);
+  public void update(List<com.example.todocloud.data.List> lists) {
+    this.lists.clear();
+    this.lists.addAll(lists);
   }
 
 }
