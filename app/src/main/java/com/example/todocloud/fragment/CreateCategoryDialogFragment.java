55915889497bebb8f7c1a8fbb27d2e1ffd4ceb12
rolandvitezhu <vitezroland91@gmail.com>
2017-07-25 15:45:49 +0200

    Simplify CreateCategoryDialogFragment class

--- app/src/main/java/com/example/todocloud/fragment/CreateCategoryDialogFragment.java
+++ app/src/main/java/com/example/todocloud/fragment/CreateCategoryDialogFragment.java
@@ -1,7 +1,9 @@
 package com.example.todocloud.fragment;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputEditText;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatDialogFragment;
@@ -11,6 +13,7 @@ import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.view.WindowManager;
 import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
@@ -24,6 +27,8 @@ public class CreateCategoryDialogFragment extends AppCompatDialogFragment {
   private TextInputLayout tilTitle;
   private TextInputEditText tietTitle;
   private ICreateCategoryDialogFragment listener;
+  private Button btnOK;
+  private Button btnCancel;
 
   @Override
   public void onAttach(Context context) {
@@ -34,27 +39,43 @@ public class CreateCategoryDialogFragment extends AppCompatDialogFragment {
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    // Beállítja az erőforrásban definiált stílust.
     setStyle(STYLE_NORMAL, R.style.MyDialogTheme);
   }
 
   @Override
-  public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                           Bundle savedInstanceState) {
+  public View onCreateView(
+      LayoutInflater inflater,
+      ViewGroup container,
+      Bundle savedInstanceState
+  ) {
     View view = inflater.inflate(R.layout.create_category, container);
-    getDialog().setTitle(R.string.itemCreateCategory);
-
-    // A footer gombok a szoftveres billentyűzet használata alatt is láthatók.
-    // A szoftveres billentyűzet nem jelenik meg alapértelmezetten.
-    if (getDialog().getWindow() != null)
-      getDialog().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE |
-          WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+    Dialog dialog = getDialog();
+    dialog.setTitle(R.string.itemCreateCategory);
+    setSoftInputMode();
 
     tilTitle = (TextInputLayout) view.findViewById(R.id.tilTitle);
     tietTitle = (TextInputEditText) view.findViewById(R.id.tietTitle);
-    final Button btnOK = (Button) view.findViewById(R.id.btnOK);
-    Button btnCancel = (Button) view.findViewById(R.id.btnCancel);
+    btnOK = (Button) view.findViewById(R.id.btnOK);
+    btnCancel = (Button) view.findViewById(R.id.btnCancel);
 
+    applyTextChangeEvent();
+    applyEditorActionEvents();
+    applyClickEvents();
+
+    return view;
+  }
+
+  private void setSoftInputMode() {
+    Dialog dialog = getDialog();
+    Window window = dialog.getWindow();
+    if (window != null) {
+      int hiddenSoftInputAtOpenDialog = WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN;
+      int softInputNotCoverFooterButtons = WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE;
+      window.setSoftInputMode(softInputNotCoverFooterButtons | hiddenSoftInputAtOpenDialog);
+    }
+  }
+
+  private void applyTextChangeEvent() {
     tietTitle.addTextChangedListener(new TextWatcher() {
 
       @Override
@@ -73,12 +94,21 @@ public class CreateCategoryDialogFragment extends AppCompatDialogFragment {
       }
 
     });
+  }
+
+  private void applyEditorActionEvents() {
     tietTitle.setOnEditorActionListener(new TextView.OnEditorActionListener() {
 
       @Override
       public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-        if ((event != null && (event.getKeyCode() == KeyEvent.KEYCODE_ENTER))
-            || (actionId == EditorInfo.IME_ACTION_DONE)) {
+        boolean pressDone = actionId == EditorInfo.IME_ACTION_DONE;
+        boolean pressEnter = false;
+        if (event != null) {
+          int keyCode = event.getKeyCode();
+          pressEnter = keyCode == KeyEvent.KEYCODE_ENTER;
+        }
+
+        if (pressEnter || pressDone) {
           btnOK.performClick();
           return true;
         }
@@ -86,23 +116,30 @@ public class CreateCategoryDialogFragment extends AppCompatDialogFragment {
       }
 
     });
+  }
+
+  private void applyClickEvents() {
     btnOK.setOnClickListener(new View.OnClickListener() {
 
       @Override
       public void onClick(View v) {
-        String title = tietTitle.getText().toString().trim();
+        String givenTitle = tietTitle.getText().toString().trim();
 
         if (validateTitle()) {
-          Category category = new Category();
-          category.setTitle(title);
-
-          category.setRowVersion(0);
-          category.setDeleted(false);
-          category.setDirty(true);
-
-          listener.onCreateCategory(category);
+          Category categoryToCreate = prepareCategoryToCreate(givenTitle);
+          listener.onCreateCategory(categoryToCreate);
           dismiss();
         }
+      }
+
+      @NonNull
+      private Category prepareCategoryToCreate(String givenTitle) {
+        Category categoryToCreate = new Category();
+        categoryToCreate.setTitle(givenTitle);
+        categoryToCreate.setRowVersion(0);
+        categoryToCreate.setDeleted(false);
+        categoryToCreate.setDirty(true);
+        return categoryToCreate;
       }
 
     });
@@ -114,16 +151,11 @@ public class CreateCategoryDialogFragment extends AppCompatDialogFragment {
       }
 
     });
-
-    return view;
   }
 
-  /**
-   * Validálja a Title mezőt.
-   * @return Kitöltött mező esetén true, egyébként false.
-   */
   private boolean validateTitle() {
-    if (tietTitle.getText().toString().trim().isEmpty()) {
+    String givenTitle = tietTitle.getText().toString().trim();
+    if (givenTitle.isEmpty()) {
       tilTitle.setError(getString(R.string.enter_title));
       return false;
     } else {
