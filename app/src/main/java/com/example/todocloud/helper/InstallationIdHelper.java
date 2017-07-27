6a3d0d43f905ca0e0e7db8d4e032236c999f3acf
rolandvitezhu <vitezroland91@gmail.com>
2017-07-27 07:35:18 +0200

    Simplify InstallationIdHelper class

--- app/src/main/java/com/example/todocloud/helper/InstallationIdHelper.java
+++ app/src/main/java/com/example/todocloud/helper/InstallationIdHelper.java
@@ -15,19 +15,16 @@ public class InstallationIdHelper {
   private static String installationId = null;
   private static final String FILE_NAME = "INSTALLATION_ID";
 
-  /**
-   * InstallationId-t olvas ki. Ha még nem létezik az installationId, akkor generáltatja.
-   * @return A kiolvasott installationId.
-   */
   public synchronized static String getInstallationId() {
     Context context = AppController.getAppContext();
     if (installationId == null) {
-      File fInstallationId = new File(context.getFilesDir(), FILE_NAME);
+      File installationIdDir = context.getFilesDir();
+      File installationIdFile = new File(installationIdDir, FILE_NAME);
       try {
-        if (!fInstallationId.exists()) {
-          writeInstallationId(fInstallationId);
+        if (!installationIdFile.exists()) {
+          writeInstallationId(installationIdFile);
         }
-        installationId = readInstallationId(fInstallationId);
+        installationId = readInstallationId(installationIdFile);
       } catch (IOException e) {
         e.printStackTrace();
       }
@@ -35,47 +32,34 @@ public class InstallationIdHelper {
     return installationId;
   }
 
-  /**
-   * Új installationId-t generáltat. Regisztráció során előfordulhatnak ütközések, ezek
-   * elkerülésére használatos a metódus.
-   * @return A generált installationId.
-   */
   public synchronized static String getNewInstallationId() {
     Context context = AppController.getAppContext();
-    File fInstallationId = new File(context.getFilesDir(), FILE_NAME);
+    File installationIdDir = context.getFilesDir();
+    File installationIdFile = new File(installationIdDir, FILE_NAME);
     try {
-      writeInstallationId(fInstallationId);
-      installationId = readInstallationId(fInstallationId);
+      writeInstallationId(installationIdFile);
+      installationId = readInstallationId(installationIdFile);
     } catch (IOException e) {
       e.printStackTrace();
     }
     return installationId;
   }
 
-  /**
-   * InstallationId-t generál és tárol.
-   * @param fInstallationId A fájl helye és neve.
-   * @throws IOException
-   */
-  private static void writeInstallationId(File fInstallationId) throws IOException {
-    FileOutputStream fileOutputStream = new FileOutputStream(fInstallationId);
-    String id = UUID.randomUUID().toString();
-    fileOutputStream.write(id.getBytes());
+  private static void writeInstallationId(File installationIdFile) throws IOException {
+    String installationIdString = UUID.randomUUID().toString();
+    byte[] installationId = installationIdString.getBytes();
+    FileOutputStream fileOutputStream = new FileOutputStream(installationIdFile);
+    fileOutputStream.write(installationId);
     fileOutputStream.close();
   }
 
-  /**
-   * Kiolvassa az installationId-t.
-   * @param fInstallationId A fájl helye és neve.
-   * @return A kiolvasott installationId.
-   * @throws IOException
-   */
-  private static String readInstallationId(File fInstallationId) throws IOException {
-    RandomAccessFile randomAccessFile = new RandomAccessFile(fInstallationId, "r");
-    byte[] bytes = new byte[(int) randomAccessFile.length()];
-    randomAccessFile.readFully(bytes);
+  private static String readInstallationId(File installationIdFile) throws IOException {
+    RandomAccessFile randomAccessFile = new RandomAccessFile(installationIdFile, "r");
+    int installationIdFileLength = (int) randomAccessFile.length();
+    byte[] installationId = new byte[installationIdFileLength];
+    randomAccessFile.readFully(installationId);
     randomAccessFile.close();
-    return new String(bytes);
+    return new String(installationId);
   }
 
 }
