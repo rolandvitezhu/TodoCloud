6fd2b7a1ae62139866f2cdc2f4b47d8d48d6e384
rolandvitezhu <vitezroland91@gmail.com>
2017-07-27 16:07:10 +0200

    Simplify ExpandableHeightListView class

--- app/src/main/java/com/example/todocloud/customcomponent/ExpandableHeightListView.java
+++ app/src/main/java/com/example/todocloud/customcomponent/ExpandableHeightListView.java
@@ -1,10 +1,14 @@
 package com.example.todocloud.customcomponent;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.ViewGroup;
 import android.widget.ListView;
 
+/**
+ * Provide proper scrolling function.
+ */
 public class ExpandableHeightListView extends ListView {
 
   boolean expanded = false;
@@ -21,10 +25,6 @@ public class ExpandableHeightListView extends ListView {
     super(context, attrs, defStyleAttr);
   }
 
-  /*public ExpandableHeightListView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
-    super(context, attrs, defStyleAttr, defStyleRes);
-  }*/
-
   public boolean isExpanded() {
     return expanded;
   }
@@ -33,25 +33,38 @@ public class ExpandableHeightListView extends ListView {
     this.expanded = expanded;
   }
 
-  /**
-   * Ha az expanded értéke true, akkor a ListView minden eleme látható, egyébként az eredeti visel-
-   * kedés valósul meg.
-   */
   @Override
   protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
 
     if (isExpanded()) {
-      // A legnagyobb érték beállítása szükséges, viszont az int 2 legnagyobb bitje nem használható
-      // fel (ezért szükséges a ">> 2").
-      int heightExpandSpec = MeasureSpec.makeMeasureSpec(Integer.MAX_VALUE >> 2,
-          MeasureSpec.AT_MOST);
-      super.onMeasure(widthMeasureSpec, heightExpandSpec);
-      ViewGroup.LayoutParams params = getLayoutParams();
-      params.height = getMeasuredHeight();
+      measureExpandedHeight(widthMeasureSpec);
+      setExpandedLayoutHeight();
     } else {
       super.onMeasure(widthMeasureSpec, heightMeasureSpec);
     }
 
+  }
+
+  /**
+   * Measure the view, using expanded height. The highest 2 bits of integer reserved for
+   * MeasureSpec mode, hence don't use those.
+   */
+  @SuppressLint("WrongCall")
+  private void measureExpandedHeight(int widthMeasureSpec) {
+    int expandedHeightMeasureSpec = MeasureSpec.makeMeasureSpec(
+        Integer.MAX_VALUE >> 2,
+        MeasureSpec.AT_MOST
+    );
+    super.onMeasure(widthMeasureSpec, expandedHeightMeasureSpec);
+  }
+
+  /**
+   * Set expanded layout height to provide proper scrolling function, using the expanded height,
+   * measured by calling measureExpandedHeight(int widthMeasureSpec) method.
+   */
+  private void setExpandedLayoutHeight() {
+    ViewGroup.LayoutParams layoutParams = getLayoutParams();
+    layoutParams.height = getMeasuredHeight();
   }
 
 }
