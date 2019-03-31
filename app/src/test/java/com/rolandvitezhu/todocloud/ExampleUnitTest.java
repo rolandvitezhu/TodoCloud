876e8327bd5ccf04d6eb9b8b5999690c2a8ce06d
drpcdoki <vitezroland91@gmail.com>
2019-03-31 16:49:45 +0200

    Fix test sources

--- app/src/test/java/com/rolandvitezhu/todocloud/ExampleUnitTest.java
+++ app/src/test/java/com/rolandvitezhu/todocloud/ExampleUnitTest.java
@@ -1,8 +1,8 @@
-package com.rolandvitezhu.todocloud.app;
+package com.rolandvitezhu.todocloud;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
