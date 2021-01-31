4cd43cb7b8ea3b2b93c4a682fd23efead1742393
drpcdoki <vitezroland91@gmail.com>
2021-01-31 13:33:22 +0100

    Fix crash starting from Android O, when creating the reminder services for the reminders at the boot of the device.

--- app/src/main/java/com/rolandvitezhu/todocloud/receiver/ReminderSetter.java
+++ app/src/main/java/com/rolandvitezhu/todocloud/receiver/ReminderSetter.java
@@ -1,8 +1,12 @@
 package com.rolandvitezhu.todocloud.receiver;
 
 import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Build;
+import android.os.IBinder;
 
 import com.rolandvitezhu.todocloud.app.AppController;
 import com.rolandvitezhu.todocloud.data.Todo;
@@ -19,7 +23,25 @@ public class ReminderSetter extends BroadcastReceiver {
   public static void createReminderServices(Context context) {
     Intent serviceIntent = new Intent(context, ReminderService.class);
     serviceIntent.setAction(ReminderService.CREATE);
-    context.startService(serviceIntent);
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+      context.startService(serviceIntent);
+      context.getApplicationContext().bindService(
+          serviceIntent,
+          new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+          //retrieve an instance of the service here from the IBinder returned
+          //from the onBind method to communicate with
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+        }
+      },
+          Context.BIND_AUTO_CREATE
+      );
+    } else
+      context.startService(serviceIntent);
   }
 
   public static void createReminderService(Todo todo) {
